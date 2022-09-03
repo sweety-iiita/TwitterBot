86007de29f071af4831b6c2a8ba904990711f61d
sweety-iiita <sweetysah95@gmail.com>
2022-09-03 21:00:35 +0530

    fix6

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2')
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Mapa@com123",
+  password: "",
   database: "PERSONAL_INFO"
 })
 
